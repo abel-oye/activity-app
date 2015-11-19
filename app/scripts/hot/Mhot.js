b3f84505b570987e8a19d2e0e81b5b95606973d3
zhangzhihao <zhangzhihao@ymatou.com>
2015-11-19 14:48:02 +0800

    M端爆款

--- app/scripts/hot/Mhot.js
+++ app/scripts/hot/Mhot.js
@@ -15,6 +15,7 @@ var picLazyLoad = (function ($, window) {
 			lazyLength = $lazyImg.not('[data-isLoad]').length;
 		//绑定window的scroll事件
 		$window.on('scroll.lazy' + guid, function () {
+			//如果容器是隐藏，则不加载
 			if ($content.css('display') == 'none') return;
 			var _winHeight = $window.height(),
 				_winScrollTop = $window.scrollTop();
@@ -22,6 +23,7 @@ var picLazyLoad = (function ($, window) {
 				var $self = $(this),
 					_offsetTop = $self.offset().top,
 					_selfHeight = $self.height();
+				//在可视区域内
 				if (_offsetTop <= (_winHeight + _winScrollTop) && _winScrollTop <= (_offsetTop + _selfHeight)) {
 					if (!$self.attr('data-isLoad')) {
 						//加上标记
@@ -42,6 +44,8 @@ var picLazyLoad = (function ($, window) {
 		}).trigger('scroll');
 	};
 })(window.jQuery || window.Zepto, window);
+
+
 
 (function (window, undefined) {
 
@@ -78,7 +82,11 @@ var picLazyLoad = (function ($, window) {
 			this.aid = aid || "";
 			this.pid = pid || "";
 			this.uuid = getUUID(aid, pid);
+
+			//加载错误
 			this.isLoadError = false;
+
+			//正在加载
 			this.isLoading = false;
 		}
 
@@ -114,9 +122,15 @@ var picLazyLoad = (function ($, window) {
 				aid = me.aid,
 				pid = me.pid,
 				cntId = '#' + me.uuid;
+
+			//如果正在加载。则返回
 			if (me.isLoading) return;
+
+			//设置为正在加载
 			me.isLoading = true;
 			me.showLoading(prevUUID);
+
+			//请求服务器数据
 			$.ajax({
 				url: 'http://api.evt.ymatou.com/ActivityTemplate/Products/aid_' + aid + '/pid_' + pid + '/ps_50',
 				dataType: 'jsonp',
@@ -124,16 +138,20 @@ var picLazyLoad = (function ($, window) {
 				jsonpCallback: 'ymatou_at_' + aid + '_' + pid,
 				success: function (data) {
 					if (data.Code != 200) {
+						//业务错误
 						me.isLoadError = true;
 						return;
 					}
 
 					var obj = data.Data;
 					if (obj.Products == null || obj.Products.length <= 0) {
+						//空数据
 						me.isLoadError = true;
 						return;
 					}
 					me.isLoadError = false;
+
+
 					obj.Products = $.map(obj.Products, function (n) {
 						//更换后缀
 						if (n.PicUrl.indexOf('/original/') > -1) {
@@ -141,23 +159,26 @@ var picLazyLoad = (function ($, window) {
 						}
 						n.PicUrl = n.PicUrl.replace('_o', '_l');
 
+						//筛选售罄
 						if (!n.SoldOut) {
 							return n;
 						}
 
 					});
-
 					obj.uuid = me.uuid;
-
+					//渲染页面
 					$tabCnt.append(_productTempl(obj));
 				},
 				complete: function () {
+					//加载完成
 					me.isLoading = false;
 					$('#ymtWrap').css('visibility', 'visible');
 					me.hideLoading(prevUUID);
 
+					//回调加载完成函数
 					me.isLoad(prevUUID);
 
+					//如果没有错误。则调用图片懒加载
 					if (!me.isLoadError) {
 						picLazyLoad(cntId);
 					}
@@ -167,11 +188,14 @@ var picLazyLoad = (function ($, window) {
 
 		CntPage.prototype.isLoad = function (prevUUID) {
 			var me = this;
-			me.callBack && me.callBack.call(me);
+			//如果不是拖动到底部自动加载，则隐藏其他 tab页面
 			if (!prevUUID) {
 				$tabCnt.find('.J_cnt').hide();
 			}
+			//显示cnt
 			$('#' + me.uuid).show();
+
+			me.callBack && me.callBack.call(me);
 		}
 
 		//显示tab页面 如果传递prevUUID则表示拖动底部自动加载
@@ -187,9 +211,11 @@ var picLazyLoad = (function ($, window) {
 		return function (aid, pid, callBack, prevUUID) {
 			var id = getUUID(aid, pid);
 
+			//加载完成回调函数
 			var callBack = $.isFunction(callBack) ? callBack : null;
 
 			if (_catch[id]) {
+				//每个UUID 只能创建单利对象
 				_catch[id].obj.callBack = callBack;
 				_catch[id].obj.show(prevUUID);
 
@@ -208,23 +234,35 @@ var picLazyLoad = (function ($, window) {
 		//当前选择的id
 		aid: '',
 		pid: '',
-		isLoadNext: true,
+
+		//当前模块索引
+		index: '',
+		//是否能继续加载下一页
+		isLoadNext: false,
+
+		//是否能隐藏tab面板
 		isHadeTab: false,
+
+		//上一个page的scroll值
 		scrollPage: {},
+
+
 		initTab: function () {
 			var me = this,
 				$subTab = $('#subTab'),
 				$tabList = $('#tabList'),
 				i, tab, subList;
+			//根据tabSetting 循环生成tab
 			for (i = 0; i < tabSetting.length; i++) {
 				tab = tabSetting[i];
-				$tabList.append('<li class="item" data-aid=' + tab.aid + '><a src="javascript:;">' + tab.name + '</a></li>');
+				$tabList.append('<li class="item" data-index=' + i + '  data-aid=' + tab.aid + '><a src="javascript:;">' + tab.name + '</a></li>');
 				var tempFn = doT.template($('#templ_subTab').html());
 				$subTab.append(tempFn({
 					subList: tab.child,
-					aid: tab.aid
+					index: i
 				}));
 
+				//子tab可滑动
 				var slidesPerView = 4
 				switch (i) {
 				case 0:
@@ -234,7 +272,7 @@ var picLazyLoad = (function ($, window) {
 					slidesPerView = 5.5;
 					break;
 				}
-				new Swiper($('#aid' + tab.aid)[0], {
+				new Swiper($('#aid' + i)[0], {
 					freeMode: true,
 					slidesPerView: slidesPerView
 				});
@@ -242,28 +280,41 @@ var picLazyLoad = (function ($, window) {
 			$subTab.find('.J_subTabCnt').eq(0).show();
 
 
-
+			//一级tab绑定事件
 			$tabList.on('click', '.item', function () {
 				var $this = $(this),
-					aid = $this.attr('data-aid');
+					aid = $this.attr('data-aid'),
+					i = $this.attr('data-index');
 				$this.addClass('cur').siblings().removeClass('cur');
-				$('#aid' + aid).show().siblings().hide();
+				//aid可能相同， 要用索引做id
+				var $sub = $('#aid' + i);
+				$sub.show().siblings().hide();
+
+				me.index = i;
 				me.aid = aid;
+
+				//出发子tab第一项的点击事件
+				$sub.find('.item').eq(0).trigger('click');
 			});
 
+			//二级tab事件
 			$subTab.on('click', '.item', function () {
 				var $this = $(this),
 					pid = $this.attr('data-pid');
+
 				$subTab.find('.item').removeClass('cur');
 				$this.addClass('cur');
 
-				if (me.pid) {
-					var uuid = getUUID(me.aid, me.pid);
-					var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-					//me.scrollPage[uuid] = scrollTop;
-				}
-
+				//重置属性
+				me.isLoadNext = false;
+				//记录这次滑动的距离
+				// if (me.pid) {
+				// 	var uuid = getUUID(me.aid, me.pid);
+				// 	var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+				// 	me.scrollPage[uuid] = scrollTop;
+				// }
 				me.pid = pid;
+				//切换页面
 				me.switchPage();
 			});
 		},
@@ -284,9 +335,9 @@ var picLazyLoad = (function ($, window) {
 			var me = this,
 				contentPage = ContentPage(me.aid, me.pid, function () {
 					me.isLoadNext = !this.isLoadError;
+					me.isHadeTab = false;
+					me.moveOldScroll();
 				});
-			me.isHadeTab = false;
-			me.moveOldScroll();
 		},
 
 		//页面上的效果
@@ -302,6 +353,8 @@ var picLazyLoad = (function ($, window) {
 					tabBoxHeight = $tabBox.height();
 				if (scrollTop > (bannerHeight + tabBoxHeight)) {
 					$tabBox[0].className = 'tab-box fixed animated short slideInDown';
+
+					//向下滚动时。隐藏头部tab
 					// if (!(p > scrollTop) && me.isHadeTab) {
 					// 	//console.log('向下滚动');
 					// 	$tabBox[0].className = 'tab-box fixed animated hinge slideOutUp';
@@ -317,11 +370,12 @@ var picLazyLoad = (function ($, window) {
 			});
 		},
 
+		//获取下一个pid
 		getNextPid: function () {
 			var me = this,
 				setting = window.tabSetting;
 			//当前aid 下 模块集合
-			var childModel = setting[_getArrayIndex(setting, 'aid', me.aid)].child;
+			var childModel = setting[me.index].child;
 
 			var index = _getArrayIndex(childModel, 'pid', me.pid);
 
@@ -339,27 +393,29 @@ var picLazyLoad = (function ($, window) {
 				_windowHeight = $window.height(),
 				isWait = false;
 
-			var wait = function () {
-				isWait = true;
-				var _st = setTimeout(function () {
-					isWait = false;
-					_st = null;
-				}, 500);
-			}
+			// //500毫秒的延迟
+			// var wait = function () {
+			// 	isWait = true;
+			// 	var _st = setTimeout(function () {
+			// 		isWait = false; 
+			// 		_st = null;
+			// 	}, 500);
+			// }
 
 			$window.on('scroll', function () {
 				var _scrollTop = $window.scrollTop(),
 					_documentHeight = $doc.height();
 
 				if ((_scrollTop >= _documentHeight - _windowHeight - 200) && !isWait && me.isLoadNext) {
-
-					//wait(); 
+					isWait = true;
 					var prevUUID = getUUID(me.aid, me.pid),
 						nextPid = me.getNextPid();
 					if (nextPid === false) {
+						isWait = false;
 						return;
 					}
 					var contentPage = ContentPage(me.aid, nextPid, function () {
+						isWait = false;
 						me.isLoadNext = !this.isLoadError;
 						me.pid = nextPid;
 						$('#' + nextPid).addClass('cur').siblings().removeClass('cur');
@@ -371,13 +427,17 @@ var picLazyLoad = (function ($, window) {
 
 		//加载默认页面
 		initDefaultPage: function () {
+			var me = this;
 			$('#subTab .item').eq(0).addClass('cur');
 			$('#tabList .item').eq(0).addClass('cur');
 			var aid = tabSetting[0].aid,
 				pid = tabSetting[0].child[0].pid;
-			this.aid = aid;
-			this.pid = pid;
-			var contentPage = ContentPage(aid, pid);
+			me.index = 0;
+			me.aid = aid;
+			me.pid = pid;
+			var contentPage = ContentPage(aid, pid, function () {
+				me.isLoadNext = !this.isLoadError;
+			});
 		},
 
 		init: function () {
